ca78f9efa28e048db83ba3a47e30dd3d39f257bf
Mohammad Reza Dehghani Tafti <dehghani735@gmail.com>
2019-09-17 23:40:23 +0430

    071 Using a helper function for Navigation

--- udemy/05Expressjs/routes/admin.js
+++ udemy/05Expressjs/routes/admin.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // Note: the first argument [path] means that the beginning of the url is like /add-product, so it should come first than / url
 router.get('/add-product', (req, res, next) => {
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); // (we are sending a response) defualt header is text/html
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
